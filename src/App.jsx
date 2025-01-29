ddce3596eeb9bb7e470be610071fa480885b2a14
shahdslaho <shahdshosho032@gmail.com>
2025-01-29 10:34:04 +0300

    your own commit

---
+++ src/App.jsx
@@ -0,0 +1,119 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const schema = z.object({
+  name: z.string(),
+  email: z.string(),
+  password: z.string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(255),
+  confirm: z.string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(255)
+}).refine((data) => data.password === data.confirm, {
+  message: "Passwords do not match",
+  path: ["confirm"], 
+});
+
+const App = () => {
+ 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+ 
+
+  const onSubmit = (data) => {
+  console.log(data)
+  };
+  return (
+    <div>
+      <form  onSubmit={handleSubmit(onSubmit)}
+        className="bg-white p-5 shadow-lg rounded-lg max-w-md mx-auto flex flex-col gap-4"
+      >
+        <div className="flex flex-col">
+          <label htmlFor="task" className="text-gray-700 font-medium">
+          Name
+          </label>
+          <input
+            {...register("name" ,{ required: " name is required" }) }
+            type="text"
+            id='name'
+            name='name'
+            placeholder="Enter your name"
+            className="border border-gray-300 rounded-lg px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.name && <p className="text-red-600 mt-1">{errors.name.message}</p>}
+        </div>
+
+
+
+
+        <div className="flex flex-col">
+          <label htmlFor="task" className="text-gray-700 font-medium">
+         Email
+          </label>
+          <input
+            {...register("email"  ,{ required: " Email is required" })}
+            type="email"
+                id='email'
+            name='email'
+            placeholder="Enter your email"
+            className="border border-gray-300 rounded-lg px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.email && <p className="text-red-600 mt-1">{errors.email.message}</p>}
+        </div>
+
+
+
+        <div className="flex flex-col">
+          <label htmlFor="task" className="text-gray-700 font-medium">
+        Password
+          </label>
+          <input
+            {...register("password" ,{ required: " Password is required" }) }
+            type="password"
+            placeholder="Enter your password"
+            className="border border-gray-300 rounded-lg px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.password && <p className="text-red-600 mt-1">{errors.password.message}</p>}
+        </div>
+
+
+
+
+        <div className="flex flex-col">
+          <label htmlFor="task" className="text-gray-700 font-medium">
+        Confirm Password
+          </label>
+          <input
+            {...register("confirm" ,{ required: "Confirm Password is required" }) }
+            type="password"
+            placeholder="Confirm Password"
+            className="border border-gray-300 rounded-lg px-3 py-2 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          {errors.confirm && <p className="text-red-600 mt-1">{errors.confirm.message}</p>}
+        </div>
+
+
+
+
+
+
+        <button
+          type="submit"
+          className="bg-blue-600 text-white font-bold px-5 py-2 rounded hover:bg-blue-700 transition"
+        >
+         Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default App
